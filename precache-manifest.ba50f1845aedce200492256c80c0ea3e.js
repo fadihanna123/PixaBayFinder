6f32730f68807275f6fd054c7443bc6c4f35bbaf
fadihanna123 <fhanna181@gmail.com>
2020-09-21 21:11:46 +0200

    Publish

---
+++ precache-manifest.ba50f1845aedce200492256c80c0ea3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6745058036779af37eda981ba73f5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a20b9809b4be0e37e19b",
+    "url": "/static/js/2.d29b7da9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d29b7da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060fb35c931a6bbf6995",
+    "url": "/static/js/main.8bbde57c.chunk.js"
+  },
+  {
+    "revision": "dcede18d1696c1b06c47",
+    "url": "/static/js/runtime-main.149e50cc.js"
+  }
+]);
